08bf8fa37c9ebaf7c4a6f068ee3cd2bc3186162b
Jona <jonathanelian64@gmail.com>
2023-01-08 19:40:51 -0700

    Add Genesis book json files and endpoint for Genesis

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-import { Hono } from "hono"
-import { serve } from "server"
+import { Context, Hono } from "hono";
+import { serve } from "server";
 
+const app = new Hono();
 
-const app = new Hono()
+app.get("/genesis/:id", async (c: Context) => {
+  const number = c.req.param("id");
+  const path = `${Deno.cwd()}/books/genesis/cap${number}.json`;
+  const chapterGenesis = await Deno.readTextFile(path);
 
-app.get('/', (c) => c.text('Hola Bible Api'))
+  return c.json(JSON.parse(chapterGenesis));
+});
 
-
-
+app.get("/", (c) => c.text("Hola Bible Api"));
 
 if (import.meta.main) {
-    serve(app.fetch)
+  serve(app.fetch);
 }
-
